061ee815f826a879889b17dd4a20361f38f8de5a
Marco Mascellino <pp47nln@eduvaud.ch>
2024-09-12 14:51:49 +0200

    feat: break on time

--- src/break.js
+++ src/break.js
@@ -1,8 +1,17 @@
 const numbers = [];
 for (let i = 0; i < 10; i++) {
-    numbers.push(Math.floor(Math.random() * 100));
+    let number = Math.floor(Math.random() * 100);
+    numbers.push(number);
+    if (number > 50) {
+        break;
+    }
 }
 
 for (let i = 0; i < numbers.length; i++) {
     console.log(numbers[i]);
+
+    if (numbers[i] > 50) {
+        console.log('Nombre plus grand que 50');
+        break;
+    }
 }
