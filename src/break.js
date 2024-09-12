0ddceea4846dc15e5d6821ccc3c48660e7fe5d14
Marco Mascellino <pp47nln@eduvaud.ch>
2024-09-12 14:53:41 +0200

    feat: break on time

--- src/break.js
+++ src/break.js
@@ -3,6 +3,7 @@ for (let i = 0; i < 10; i++) {
     let number = Math.floor(Math.random() * 100);
     numbers.push(number);
     if (number > 50) {
+        console.log(`Nous avon eviter ${9-i} repetition de boucle inutile`);
         break;
     }
 }
