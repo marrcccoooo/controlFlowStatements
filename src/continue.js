8b8257d67b238d23a340d0a17a3d032bd5b24766
Marco Mascellino <pp47nln@eduvaud.ch>
2024-09-13 14:13:40 +0200

    feat: continue better

--- src/continue.js
+++ src/continue.js
@@ -3,6 +3,17 @@ for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
 }
 
+let bigNumber = 0;
+let result = 0;
+
+
 for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
+    if (bigNumber < numbers[i]) {
+        bigNumber = numbers[i];
+        result += numbers[i];
+        continue;
+    }
 }
+
+
+console.log(bigNumber, result);
