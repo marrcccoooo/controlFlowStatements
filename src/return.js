380ecc1ee41954c3938b67bf15cd19d8bc9c6768
Marco Mascellino <pp47nln@eduvaud.ch>
2024-09-26 14:48:28 +0200

    feat: return asap

--- src/return.js
+++ src/return.js
@@ -2,7 +2,26 @@ const numbers = [];
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
 }
+let number = 100;
+let lastsNumber = [];
 
-for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
+function boucle() {
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] in lastsNumber) {
+            return number;
+        }
+        if (numbers[i] % 2 === 0) {
+            number -= numbers[i];
+            lastsNumber.push(numbers[i]);
+        }
+        if (i === numbers.length / 2) {
+            return number;
+        }
+        if (number === 50) {
+            return number;
+        }
+    }
 }
+
+boucle();
+console.log(number, lastsNumber);
